91549ad1755fd725e8aaf09ccc069fdfb7f1ac9a
Solrac_B <carlitosaburgosj1609@gmail.com>
2023-03-27 08:54:21 -0500

    update

---
+++ example/welcome.php
@@ -0,0 +1,10 @@
+<?php
+    //This is my first php file 
+    echo "Welcome, this ia a php file \n";
+    $num1 = 10;
+    $num2 = 20;
+    $suma= $num1 + $num2;
+    $my_name=" fhd ";
+    echo "hola  ".$my_name;
+    echo "The answer is: ".$suma;
+?>
